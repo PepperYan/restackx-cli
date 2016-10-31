c00b7d33146ab38b4292f50f9b57add6f264cbdf
hibad <coldwinner1@163.com>
2016-10-31 18:36:55 +0800

    try again

--- prototype/modules/restack-core/lib/store/configureStore.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/*不能合并,根据编译时状态生成js*/
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod');
-} else {
-  module.exports = require('./configureStore.dev');
-}
