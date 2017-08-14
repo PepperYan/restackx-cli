b9063ba39d70970950d3ae296f671685d94c5cf6
hibad1 <coldwinner1@163.com>
2017-08-14 17:27:24 +0800

    移除restackx-core依赖

--- prototype/src/js/index.jsx
+++ prototype/src/js/index.jsx
@@ -1,20 +1,30 @@
 import React,{Component} from 'react'
 import { render } from 'react-dom'
 import routes from './routes'
-import Store from './stores'
-import {App} from 'restackx-core'
+import stores from './stores'
+import { BrowserRouter } from 'react-router-dom'
+import {Provider} from 'mobx-react'
 
+function App(){
+  return (
+    <BrowserRouter>
+      <Provider {...stores}>
+        {routes}
+      </Provider>
+    </BrowserRouter>
+  )
+}
 
 const container = document.getElementById('container');
 render(
-  <App store={Store} routes={routes}/>,
+  <App />,
   container
 )
 
 if(module.hot){
   module.hot.accept('./routes', () => {
     render(
-      <App store={Store} routes={routes}/>,
+      <App />,
       container
     )
   });
--- prototype/src/js/stores.js
+++ prototype/src/js/stores.js
@@ -1,5 +1,18 @@
 import {observable, computed, reaction} from 'mobx'
-import {handleModels} from 'restackx-core'
+
+function handleModels(context){
+  const models = {}
+  const preHandle =  context.keys()
+  .map((key)=>{
+    const array = key.split("/")
+    const fullName = array[array.length-1]
+    const name = array[array.length-1].slice(0,fullName.length-9)
+    const modelCls = context(key).default
+    return models[name] = new modelCls()
+  })
+  return models;
+}
+
 
 const modelContext = require.context('./', true, /.store.js$/)
 var models = handleModels(modelContext)
